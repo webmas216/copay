fee4e19b10bd0a0516551431cc3c7f25d6e0c2d2
root <root@wallet.polispay.org>
2018-01-28 23:20:36 +0000

    Change Wallet direction

--- src/js/services/configService.js
+++ src/js/services/configService.js
@@ -14,7 +14,7 @@ angular.module('copayApp.services').factory('configService', function(storageSer
 
     // Bitcore wallet service URL
     bws: {
-      url: 'https://bws.bitpay.com/bws/api',
+      url: 'https://bws.polispay.org/bws/api',
     },
 
     download: {
