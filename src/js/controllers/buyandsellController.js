bd6c1e5a8f49571c57e3c8a2674600eea3061d17
webmas216 <tyefred210@gmail.com>
2018-02-07 13:09:28 -0600

    Updated buyandsell page

--- src/js/controllers/buyandsellController.js
+++ src/js/controllers/buyandsellController.js
@@ -3,9 +3,24 @@
 angular.module('copayApp.controllers').controller('buyandsellController', function($scope, $ionicHistory, buyAndSellService, lodash) {
 
   $scope.$on("$ionicView.beforeEnter", function(event, data) {
-    $scope.services = buyAndSellService.get();
+    // $scope.services = buyAndSellService.get();
 
-    if (lodash.isEmpty($scope.services))
-      $ionicHistory.goBack();
+    // if (lodash.isEmpty($scope.services))
+    //   $ionicHistory.goBack();
+
+    $scope.services = [
+      {
+        url : 'https://www.southxchange.com/Market/Book/POLIS/BTC',
+        caption : 'Southxchange'
+      },
+      {
+        url : 'https://stocks.exchange/trade/POLIS/BTC',
+        caption : 'Stocks.exchange'
+      },
+      {
+        url : 'https://www.cryptopia.co.nz/',
+        caption : 'Cryptopia'
+      }
+    ]
   });
 });
